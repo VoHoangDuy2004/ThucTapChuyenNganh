2ba7c8d5a81fe0312e546bad1ac49297c490a458
VoHoangDuy2004 <vohoangduyanh24122003@gmail.com>
2025-11-02 21:41:32 +0700

    Day bai tap layout len GitHub

---
+++ src/main/java/com/example/demo/BaitapTuan1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 	BaitapTuan1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaitapTuan1Application.class, args);
+	}
+
+}
